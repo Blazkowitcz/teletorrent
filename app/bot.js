ef5dd6888472e5e5e1f6d3c8592756f6b3bf7cb9
Blazkowicz <bonnet.nicolas66@gmail.com>
2021-03-12 18:14:25 +0100

    Init bot

---
+++ app/bot.js
@@ -0,0 +1,16 @@
+const TelegramBot = require('node-telegram-bot-api');
+const config = require('../config.json');
+const token = config.bot_token;
+var bot = null;
+
+/**
+ * Init bot
+ */
+function start() {
+    bot = new TelegramBot(token, {polling: true});
+    bot.onText(/\/start/, (msg) => {
+        bot.sendMessage(msg.chat.id, "Greeting " + msg.chat.username);
+    });
+}
+
+module.exports = { start }
