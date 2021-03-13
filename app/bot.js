4431692f795d82d177b2459d6f747a9d2a8f405d
Blazkowicz <bonnet.nicolas66@gmail.com>
2021-03-13 13:05:34 +0100

    Create tracker

--- app/bot.js
+++ app/bot.js
@@ -2,7 +2,9 @@ const TelegramBot = require('node-telegram-bot-api');
 const config = require('../config.json');
 const token = config.bot_token;
 var database = require('./database/index');
+const Tracker = require('./database/models/tracker.model');
 var bot = null;
+var tracker_list = [];
 
 /**
  * Init bot
@@ -12,6 +14,31 @@ function start() {
     bot.onText(/\/start/, (msg) => {
         bot.sendMessage(msg.chat.id, "Greeting " + msg.chat.username);
     });
+    setTrackerCommands(bot);
+    
+}
+
+/**
+ * Set tracker command
+ * @param {Object} bot 
+ */
+function setTrackerCommands(bot){
+    bot.onText(/\/tracker add (.+)/, (msg, info) => {
+        elements = info[1].split(' ');
+        if(elements.length < 2){
+            bot.sendMessage(msg.chat.id, "Missing arguments");
+            return null;
+        }else {
+            Tracker.create(new Tracker(elements[0], elements[1], elements[2] || null, elements[3] || null), (err) => {
+                bot.sendMessage(msg.chat.id, "Tracker added");
+            });
+            //tracker_list.push(new Tracker(elements[0], elements[1], elements[2] || null, elements[3] || null));
+            
+        }
+    })
+    bot.onText(/\/tracker list/, (msg, info) => {
+        console.log(tracker_list);
+    })
 }
 
 module.exports = { start }
--- app/database/models/tracker.model.js
+++ app/database/models/tracker.model.js
@@ -2,12 +2,29 @@
  * Constructor
  * @param {JSON} datas 
  */
- function Search(datas) {
-    for (var key in datas) {
-        this[key] = datas[key];
-    }
+function Tracker(name, address, rss_address, passkey) {
+    this.name = name;
+    this.address = address;
+    this.rss_address = rss_address || null;
+    this.passkey = passkey || null;
 };
 
-Search.Generate = "CREATE TABLE IF NOT EXISTS trackers (ID INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, address TEXT, rss_address TEXT, passkey INTEGER)";
+/**
+ * Create new tracker
+ * @param {Object} tracker 
+ * @param {Function} callback 
+ */
+Tracker.create = function create(tracker, callback) {
+    var db = require('../../database');
+    db.run("INSERT INTO trackers (name, address, rss_address, passkey) VALUES (?,?,?,?)", tracker.name, tracker.address, tracker.rss_address, tracker.passkey, (err) => {
+        if(err) 
+            callback(err);
+        else 
+            callback(null);
+    });
+}
 
-module.exports = Search;
+/** Database table generator */
+Tracker.Generate = "CREATE TABLE IF NOT EXISTS trackers (ID INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, address TEXT, rss_address TEXT, passkey INTEGER)";
+
+module.exports = Tracker;
